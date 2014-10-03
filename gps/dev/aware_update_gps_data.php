120b29b80a43dbcfca5fb4ee6bc47627f3923546
Clifton Sweeney <csweeney@myjobready.com>
2014-10-03 11:20:39 -0400

    Aware Update GPS Data
    Added `TimeStamp` variable to the GPSDataTruckTable to receive the time
    information.

--- gps/dev/aware_update_gps_data.php
+++ gps/dev/aware_update_gps_data.php
@@ -4,25 +4,25 @@
     require_once('../lib/Relationship.php');
     require 'aware_cred.php';
     require 'aware_curl.php';
-    
+
     $truckId = 50;
-    
+
     $data = json_encode(array(
         "username" => $username,
         "password" => $password
     ));
-    
-    $dump = json_decode(post_curl($mapURL, $data), true);
-    
-    if ($dump['responseType'] !== 'Success'){
-        
-    }
-    
-    $positions = $dump['data']['positions'];
-    
-    //update only driverId 65600 to truck 50
 
-    for ($i = 0; $i < count($positions); $i++) {
+    $dump = json_decode(post_curl($mapURL, $data), true);
+
+    if ($dump['responseType'] !== 'Success')
+    {
+
+    }
+
+    $positions = $dump['data']['positions'];
+    //update only driverId 65600 to truck 50
+    for ($i = 0; $i < count($positions); $i++)
+    {
         $device = $positions[$i];
         $driverId = $device['driverId'];
         $date = $device['date'];
@@ -34,25 +34,22 @@
         $speeding = $device['speeding'];
         $behaviour = $device['behaviorCd'];
         $estSpeedLimit = $device['estSpeedLimit'];
-        
-        if ($driverId == '65600'){
+
+        if ($driverId == '65600')
+        {
             GPSData($truckId, $driverId, $date/1000, $lat, $lng);
-            GPSDataTruck($truckId, $driverId, $heading, $direction, $speed, $speeding, $behaviour, $estSpeedLimit);
+            GPSDataTruck($truckId, $driverId, $date/1000, $heading, $direction, $speed, $speeding, $behaviour, $estSpeedLimit);
         }
-        
     }
-    
-    function GPSData($Truck, $GPSID, $TimeStamp, $Latitude, $Longitude){
+
+    function GPSData($Truck, $GPSID, $TimeStamp, $Latitude, $Longitude)
+    {
         $time = date('Y-m-d, H:i:s', $TimeStamp);
-
-        $sql = "
-            INSERT
-                    INTO GPSData
-                    (Truck, GPSID, TimeStamp, Latitude, Longitude)
-            VALUES
+        $sql = "INSERT INTO GPSData
+                	(Truck, GPSID, TimeStamp, Latitude, Longitude)
+            	VALUES
                     (:truck, :gpsid, :timestamp, :lat, :long)
-            ";
-
+            	";
         $params = array(
             ':truck'=>$Truck,
             ':gpsid'=>$GPSID,
@@ -60,24 +57,22 @@
             ':lat'=>$Latitude,
             ':long'=>$Longitude
         );
-
         pdo_execute_query($sql, $params);
-
         echo "<p>GPSData updated $time</p>";
     }
-    
-    function GPSDataTruck($Truck, $GPSID, $Heading, $Direction, $Speed, $Speeding, $Behaviour, $EstSpeedLimit){
-        $sql = "
-            INSERT
-                    INTO GPSDataTruck
-                    (Truck, GPSID, Heading, Direction, Speed, Speeding, Behaviour, EstSpeedLimit)
-            VALUES
-                    (:truck, :gpsid, :heading, :direction, :speed, :speeding, :behaviour, :estspeedlimit)
-            ";
 
+    function GPSDataTruck($Truck, $GPSID, $TimeStamp, $Heading, $Direction, $Speed, $Speeding, $Behaviour, $EstSpeedLimit)
+    {
+    	$time = date('Y-m-d, H:i:s', $TimeStamp);
+        $sql = "INSERT INTO GPSDataTruck
+                    (Truck, GPSID, TimeStamp, Heading, Direction, Speed, Speeding, Behaviour, EstSpeedLimit)
+            	VALUES
+                    (:truck, :gpsid, :timestamp, :heading, :direction, :speed, :speeding, :behaviour, :estspeedlimit)
+        		";
         $params = array(
             ':truck'=>$Truck,
             ':gpsid'=>$GPSID,
+            ':timestamp'=>$time,
             ':heading'=>$Heading,
             ':direction'=>$Direction,
             ':speed'=>$Speed,
@@ -85,10 +80,8 @@
             ':behaviour'=>$Behaviour,
             ':estspeedlimit'=>$EstSpeedLimit
         );
-
         pdo_execute_query($sql, $params);
+        echo "<p>GPSDataTruck updated for $Truck $GPSID.</p>";
 
-        echo "<p>GPSDataTruck updated</p>";
     }
-
 ?>
